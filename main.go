88c205eb182d2b0cebb43bf753ceeeb9a1515952
Nick Graffis <nicholasgraffis@gmail.com>
2021-08-07 18:10:02 -0700

    test

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"fmt"
+	"github.com/buckket/go-blurhash"
+	"image/png"
+	"os"
+	"net/http"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	imageFile, _ := http.Get("test.png")
+	loadedImage, err := png.Decode(imageFile)
+	str, _ := blurhash.Encode(4, 3, loadedImage)
+	if err != nil {
+		// Handle errors
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body: str,
 	}, nil
 }
 
