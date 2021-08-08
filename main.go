50c0132816d6ead5dfc26553621d59a3173579ac
Nick Graffis <nicholasgraffis@gmail.com>
2021-08-07 18:31:36 -0700

    test

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	imageFile, _ := http.Get("test.png")
-	loadedImage, err := png.Decode(imageFile)
-	str, _ := blurhash.Encode(4, 3, loadedImage)
+	res, err = http.Get("https://i.natgeofe.com/n/622db4d3-6f73-462d-b08f-4fb528467f50/01-flamingo-friendships-nationalgeographic_1477297.jpg")
+	if err != nil || res.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body: err,
+		}, nil
+	}
+	defer res.Body.Close()
+	m, _, err := image.Decode(res.Body)
 	if err != nil {
-		// Handle errors
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body: err,
+		}, nil
 	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: str,
+		Body: m,
 	}, nil
 }
 
